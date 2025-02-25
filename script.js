6b46a5bddc3979e9bd3557c3bb6860f384626ebc
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-25 10:27:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	str.split(' ').sort().join(' ');
 	for(let i=1 ; i<str.length ; i=i+2){
 		if(str[i] != str[i-1]){
 			return str[i-1];
 		}
 		
-	}return null;
 	}
+	return null;
+}
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
